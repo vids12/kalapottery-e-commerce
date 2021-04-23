9a7e00bddbbac587ea2914ad4de55e2804cb3f91
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-04-23 17:39:33 +0530

    added:router

---
+++ src/pages/404.js
@@ -0,0 +1,6 @@
+export function NotFound(){
+    return <>
+        <h1 style={{textAlign:"center",fontSize:"20rem"}}>404</h1>
+        <h3 style={{textAlign:"center"}}>Page Not Found</h3>
+    </>
+}
