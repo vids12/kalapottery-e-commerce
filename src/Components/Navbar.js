276ed1e41a01f16ef6b8a61e90535a07d7d6ca66
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-04-25 20:33:19 +0530

    styled: responsive nav-bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../dataProvider/CartProvider";
 import { SearchBar } from "./SearchBar";
 
 export function Navbar() {
+  // const [openSidebar,setOpenSidebar] = useState();
   const {itemsInCart, wishList} = useCart();
   return <div className="nav-bar">
     <img
         src="https://themeforshop.github.io/kala-pottery-demo/images/logo.png"
         alt="logo"
       />
-    <nav>
-      <Link to="/"> Home </Link>
-      <Link to="/products"> Products </Link>
-      <Link to="/cart">Cart <span>{itemsInCart.length}</span></Link>
-      <Link to="/wishlist">WishList <span>{wishList.length}</span></Link>
-      <SearchBar />
-    </nav>
+      <div className="btn">
+        <img src="https://www.flaticon.com/svg/vstatic/svg/3388/3388829.svg?token=exp=1619272488~hmac=31cd8170d45da88e513d33e2007c3b2c"  alt="hamburger-icon" className="ham"/>
+      <nav className="sidebar">
+        <Link to="/"> Home </Link>
+        <Link to="/products"> Products </Link>
+        <Link to="/cart">Cart <span>{itemsInCart.length}</span></Link>
+        <Link to="/wishlist">WishList <span>{wishList.length}</span></Link>
+        <SearchBar />
+      </nav>
+      </div>
   </div>
 }
