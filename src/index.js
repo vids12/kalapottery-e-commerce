d9d9c39679f499d9f387920a70f2bf08c197baa0
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-05-02 15:52:19 +0530

    deleted: mockserver

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import { CartProvider } from "./dataProvider/CartProvider";
-// import setupMockServer from "./api/mock.server";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
-// setupMockServer();
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
