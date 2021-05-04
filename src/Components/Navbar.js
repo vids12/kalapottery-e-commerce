7e4b7f8540baf798a9f3e83b0b679773ad73d1bb
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-05-04 18:55:40 +0530

    refactored:search bar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import { useCart } from "../dataProvider/CartProvider";
 import { SearchBar } from "./SearchBar";
 
 export function Navbar() {
-  const {itemsInCart, wishList} = useCart();
+  const {itemsInCart, wishList, setShowSearchPage, showSearchPage} = useCart();
   return <div className="nav-bar">
     <img
         src="https://themeforshop.github.io/kala-pottery-demo/images/logo.png"
@@ -12,13 +12,14 @@ export function Navbar() {
       />
       <div className="btn">
         <img src="https://www.flaticon.com/svg/vstatic/svg/3388/3388829.svg?token=exp=1619272488~hmac=31cd8170d45da88e513d33e2007c3b2c"  alt="hamburger-icon" className="ham"/>
-      <nav className="sidebar">
-        <NavLink end to="/" activeStyle={{fontWeight : "bold"}} activeClassName="selected"> Home </NavLink>
-        <NavLink to="/products" activeStyle={{fontWeight : "bold"}}> Products </NavLink>
-        <NavLink to="/cart" activeStyle={{fontWeight : "bold"}}>Cart <span>{itemsInCart.length}</span></NavLink>
-        <NavLink to="/wishlist" activeStyle={{fontWeight : "bold"}}>WishList <span>{wishList.length}</span></NavLink>
-        <SearchBar />
-      </nav>
+        <nav className="sidebar">
+          <i className="fas fa-search" onClick={()=>setShowSearchPage(true)}>Search</i>
+          {showSearchPage && <SearchBar />}
+          <NavLink end to="/" activeStyle={{fontWeight : "bold"}} activeClassName="selected"> Home </NavLink>
+          <NavLink to="/products" activeStyle={{fontWeight : "bold"}}> Products </NavLink>
+          <NavLink to="/cart" activeStyle={{fontWeight : "bold"}}>Cart <span>{itemsInCart.length}</span></NavLink>
+          <NavLink to="/wishlist" activeStyle={{fontWeight : "bold"}}>WishList <span>{wishList.length}</span></NavLink>
+        </nav>
       </div>
   </div>
 }
