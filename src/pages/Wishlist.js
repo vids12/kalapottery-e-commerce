c00ac2642a0af3370d021ba2067409cd07db018e
Vidushi Agrawal <67853687+vids12@users.noreply.github.com>
2021-05-04 18:56:09 +0530

    styled: wishlist page

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -2,21 +2,24 @@ import { useCart } from "../dataProvider/CartProvider";
 
 export function WishList() {
   const { wishList,dispatch: wishlistDispatch } = useCart();
-  return (
-    <ul className="card-div">
-      {wishList.map((obj) => {
-        return (
-          <div className="card">
-            <img src={obj.image} alt={obj.name} className="product-img" />
-            <li style={{ margin: "0.5rem" }}>{obj.name}</li>
-            <p style={{ margin: "0.3rem" }}>{obj.price}</p>
-            <button onClick={() => wishlistDispatch({type:"REMOVE_FROM_WISHLIST",payload: obj})} 
-            className="secondary-btn">
-              Remove
-            </button>
-          </div>
-        );
-      })}
+  return <div>
+        <div className="bg-img cart-bgImg">
+          <h2 className="page-heading">WishList</h2>
+        </div>
+        <ul className="card-div">
+        {wishList.map((obj) => {
+          return (
+            <div className="card">
+              <img src={obj.image} alt={obj.name} className="product-img" />
+              <li style={{ margin: "0.5rem" }}>{obj.name}</li>
+              <p style={{ margin: "0.3rem" }}>{obj.price}</p>
+              <button onClick={() => wishlistDispatch({type:"REMOVE_FROM_WISHLIST",payload: obj})} 
+              className="secondary-btn">
+                Remove
+              </button>
+            </div>
+          );
+        })}
     </ul>
-  );
+  </div>
 }
